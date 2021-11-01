d4c2900e4f33ed4973d65f1424ef354052a66688
Angel Cid <angel.cid@gmail.com>
2021-11-01 14:14:27 +0100

    añadiendo Email

---
+++ src/Email.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Jakala\ValueObjects;
+
+use ValueObject;
+
+final class Email implements ValueObject, \Stringable
+{
+
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+    }
+
+    public function value(): mixed
+    {
+        // TODO: Implement value() method.
+    }
+}
